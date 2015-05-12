a325d6dc5ba47d53577815cfc1e2a1117c841d5c
Magnetic Duck <zenmags@gmail.com>
2015-05-12 06:48:04 +0000

    styling, about page

--- index.php
+++ index.php
@@ -17,7 +17,11 @@
         <div class="head"> </div>
 
         <div class="row">
-          <p> Oh, hi! I wasn't expecting guests. There's a decent amount of white space under this text, so I hope you have a proportional amount of imagination. </p>
+        <!--
+          <p> There was once a brief moment in my life when I believed I could capture some sort of essential truths in writing. All that remains from those attempts is a lingering aftertaste of exasperation and a what I believe to be a knowledge that the best anyone can do in the face of reality is to glorify its absurtity, paint its chaos as a Starry Sky. The resourceful farmer says nothing exists for less than a handful of reasons. The tired artist proclaims reality a raging river, destining all our work and all our troubles to be carried away, denying us so much as a place to stand. The mathematician turns the morning's brew into lemmas. The aspiring hacker attempts nothing more than to grok at least a bit of it. A few strokes of grey here, a dab of blue there. What more can we ask for than that; what more could we desire than to look up to the night sky and remember ourselves, futile and passing, but at least as vast and mysterious as the very things that puzzle us. </p>
+          <p> But I don't feel like going to the starry sky. Not yet, anyway. Because today I'm going to make a little piece of something my own. </p>
+          -->
+          <p> I'm MagneticDuck, and this is my personal site. There are things here sometimes, hopefully they'll be of use somehow. </p>
         </div>  
         <div class="row">
         </div>
