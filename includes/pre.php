3c8d999e80862c4c1689d5a29afa3f434a6e2f6d
Magnetic Duck <zenmags@gmail.com>
2015-05-23 21:54:35 +0000

    changed doctype

--- includes/pre.php
+++ includes/pre.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd"> 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd"> 
 <html xmlns="http://www.w3.org/1999/xhtml">
   <head>
     <link type="text/css" rel="stylesheet" href="/includes/css/nav.css"/>
